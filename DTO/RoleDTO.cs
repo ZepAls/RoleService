617fe125afe75645ab1ad410b289683b8a3cad78
ZepAlsters <94059018+ZepAlsters@users.noreply.github.com>
2024-05-23 13:50:04 +0200

    Add project files.

---
+++ DTO/RoleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DTO
+{
+    public class RoleDTO
+    {
+        public int Id { get; set; }
+        public string CharacterPageName { get; set; }
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
